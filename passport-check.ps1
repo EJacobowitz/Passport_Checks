466501f6d79cd0e8470862bcdc41efab4313c77c
Jacobowitz, Eric <EJacobowitz@jetblue.com>
2021-02-02 23:41:59 -0500

    Update to format and event logging.

--- passport-check.ps1
+++ passport-check.ps1
@@ -7,13 +7,14 @@ $results = Get-odbcdata -DSN crewdata -query $query1
 if ($null -ne $results) {
     foreach ($record in $results) {
         $newdate = "20" + $record.Passport_ExpireDate.Substring(2, 6)
+        $current_exdate = $record.Passport_ExpireDate.ToString()
         $employeenum = $record.Emplno.ToString()
         $updateid = $record.Updateid_Updempno.ToString()
         $updatedt = $record.Updateid_Upddate.tostring()
         $query2 = "update EP set Passport_ExpireDate = $newdate where Emplno = $employeenum"
         Get-odbcdata -DSN crewdata -query $query2
-        $message = "$($employeenum) had wrong passport expiration date of $($record.Emplno.ToString()) and was changed to $($newdate). The last person to update the crewmember was $($updateid) on $($updatedt)."
-        write-customlog -eventlogname Application -EventSource "Crewtrac-Passport" -EventMessage $message
+        $message = "$($employeenum) had wrong passport expiration date of $($current_exdate) and was changed to $($newdate). The last person to update the crewmember was $($updateid) on $($updatedt)."
+        write-customlog -eventlogname Application -EventSource "Crewtrac-Passport" -EventMessage $message -EventNum 9991
     }
 }
 
@@ -22,9 +23,9 @@ $today = (get-date -Format yyyyMMdd).ToString()
 $query3 = "select EM.Emplno, concat(concat(rtrim(EM.emplname_Lname), ', '), rtrim(EM.emplname_Fname)) as name, EM.Status, EP.passport_number, EP.Passport_ExpireDate, EP.Passport_OID, EP.Updateid_Updempno, EP.Updateid_Upddate from EM, EP where EM.Emplno = EP.Emplno and EM.status <> 'I' and EP.Passport_ExpireDate < '$($today)';"
 $PEresults = Get-odbcdata -DSN crewdata -query $query3
 if ($null -ne $PEresults) {
-    $data = $PEresults | Format-Table -AutoSize | ConvertTo-Html -Fragment
+    $data = $PEresults | Select-Object -Property Emplno, name, Status, Passport_Number, Passport_ExpireDate, Passport_OID, Updateid_Updempno, Updateid_Upddate  | ConvertTo-Html -Fragment
     $Report = @"
-    <b><p style="font-size:50px">Crewtrac Passport changes</p></b><br>
+    <b><p style="font-size:50px">Crewtrac Expired Passport</p></b><br>
     $($data)
 "@
 
